5781466ea052ef9ba5b241383b34614faa6843c5
RumitPatel <rumit.patel5@gmail.com>
2023-12-26 16:05:27 +0530

    kotlin synthetic removed.

--- app/src/main/java/com/rumit/tabchangeonscroll/WIthScrollViewActivity.kt
+++ app/src/main/java/com/rumit/tabchangeonscroll/WIthScrollViewActivity.kt
@@ -9,47 +9,42 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.rumit.tabchangeonscroll.adapters.TitlesAdapter
+import com.rumit.tabchangeonscroll.databinding.ActivityWithScrollviewBinding
 import com.rumit.tabchangeonscroll.util.TAG
 import com.rumit.tabchangeonscroll.util.getTitles
-import kotlinx.android.synthetic.main.activity_with_scrollview.sv
-import kotlinx.android.synthetic.main.activity_with_scrollview.tvTitles
-import kotlinx.android.synthetic.main.test_data_view_1.llType0
-import kotlinx.android.synthetic.main.test_data_view_1.tvContentTitle1
-import kotlinx.android.synthetic.main.test_data_view_2.llType1
-import kotlinx.android.synthetic.main.test_data_view_2.tvContentTitle2
-import kotlinx.android.synthetic.main.test_data_view_3.llType2
-import kotlinx.android.synthetic.main.test_data_view_3.tvContentTitle3
 
 
 class WIthScrollViewActivity : AppCompatActivity() {
 
     private lateinit var mContext: Context
+    private lateinit var binding: ActivityWithScrollviewBinding
 
     private var titlesAdapter: TitlesAdapter? = null
     private var selectedTitlePosition: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_with_scrollview)
+        binding = ActivityWithScrollviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         mContext = this
 
-        tvTitles.apply {
+        binding.tvTitles.apply {
             layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false)
-            titlesAdapter = TitlesAdapter(getTitles()) {
+            titlesAdapter = TitlesAdapter(mContext, getTitles()) {
                 selectedTitlePosition = it
                 setTitleSelected()
 
                 when (selectedTitlePosition) {
                     0 -> {
-                        sv.scrollTo(0, llType0.top)
+                        binding.sv.scrollTo(0, binding.testDataView0.llType0.top)
                     }
 
                     1 -> {
-                        sv.scrollTo(0, llType1.top)
+                        binding.sv.scrollTo(0, binding.testDataView1.llType1.top)
                     }
 
                     2 -> {
-                        sv.scrollTo(0, llType2.top)
+                        binding.sv.scrollTo(0, binding.testDataView2.llType2.top)
                     }
                 }
             }
@@ -58,58 +53,52 @@ class WIthScrollViewActivity : AppCompatActivity() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val scrollBounds = Rect()
-            sv.getHitRect(scrollBounds)
+            binding.sv.getHitRect(scrollBounds)
 
-            sv.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-                if (tvContentTitle1 != null) {
-                    if (tvContentTitle1.getLocalVisibleRect(scrollBounds)) {
-                        if (!tvContentTitle1.getLocalVisibleRect(scrollBounds)
-                            || scrollBounds.height() < tvContentTitle1.height
-                        ) {
-                            Log.e(TAG, "tvContentTitle1 displayed partial portion")
-                        } else {
-                            Log.e(TAG, "tvContentTitle1 displayed full portion")
-                        }
-                        selectedTitlePosition = 0
-                        setTitleSelected()
-
+            binding.sv.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                if (binding.testDataView0.tvContentTitle1.getLocalVisibleRect(scrollBounds)) {
+                    if (!binding.testDataView0.tvContentTitle1.getLocalVisibleRect(scrollBounds)
+                        || scrollBounds.height() < binding.testDataView0.tvContentTitle1.height
+                    ) {
+                        Log.e(TAG, "tvContentTitle1 displayed partial portion")
                     } else {
-                        Log.e(TAG, "tvCatTextISF4 No")
+                        Log.e(TAG, "tvContentTitle1 displayed full portion")
                     }
+                    selectedTitlePosition = 0
+                    setTitleSelected()
+
+                } else {
+                    Log.e(TAG, "tvCatTextISF4 No")
                 }
 
-                if (tvContentTitle2 != null) {
-                    if (tvContentTitle2.getLocalVisibleRect(scrollBounds)) {
-                        if (!tvContentTitle2.getLocalVisibleRect(scrollBounds)
-                            || scrollBounds.height() < tvContentTitle2.height
-                        ) {
-                            Log.e(TAG, "tvContentTitle2 displayed partial portion")
-                        } else {
-                            Log.e(TAG, "tvContentTitle2 displayed full portion")
-                        }
-                        selectedTitlePosition = 1
-                        setTitleSelected()
-
+                if (binding.testDataView1.tvContentTitle2.getLocalVisibleRect(scrollBounds)) {
+                    if (!binding.testDataView1.tvContentTitle2.getLocalVisibleRect(scrollBounds)
+                        || scrollBounds.height() < binding.testDataView1.tvContentTitle2.height
+                    ) {
+                        Log.e(TAG, "tvContentTitle2 displayed partial portion")
                     } else {
-                        Log.e(TAG, "tvCatTextISF5 No")
+                        Log.e(TAG, "tvContentTitle2 displayed full portion")
                     }
+                    selectedTitlePosition = 1
+                    setTitleSelected()
+
+                } else {
+                    Log.e(TAG, "tvCatTextISF5 No")
                 }
 
-                if (tvContentTitle3 != null) {
-                    if (tvContentTitle3.getLocalVisibleRect(scrollBounds)) {
-                        if (!tvContentTitle3.getLocalVisibleRect(scrollBounds)
-                            || scrollBounds.height() < tvContentTitle3.height
-                        ) {
-                            Log.e(TAG, "tvContentTitle3 displayed partial portion")
-                        } else {
-                            Log.e(TAG, "tvContentTitle3 displayed full portion")
-                        }
-                        selectedTitlePosition = 2
-                        setTitleSelected()
-
+                if (binding.testDataView2.tvContentTitle3.getLocalVisibleRect(scrollBounds)) {
+                    if (!binding.testDataView2.tvContentTitle3.getLocalVisibleRect(scrollBounds)
+                        || scrollBounds.height() < binding.testDataView2.tvContentTitle3.height
+                    ) {
+                        Log.e(TAG, "tvContentTitle3 displayed partial portion")
                     } else {
-                        Log.e(TAG, "tvCatTextISF6 No")
+                        Log.e(TAG, "tvContentTitle3 displayed full portion")
                     }
+                    selectedTitlePosition = 2
+                    setTitleSelected()
+
+                } else {
+                    Log.e(TAG, "tvCatTextISF6 No")
                 }
             })
         }
--- app/src/main/java/com/rumit/tabchangeonscroll/adapters/TitlesAdapter.kt
+++ app/src/main/java/com/rumit/tabchangeonscroll/adapters/TitlesAdapter.kt
@@ -1,65 +1,71 @@
 package com.rumit.tabchangeonscroll.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.rumit.tabchangeonscroll.R
+import com.rumit.tabchangeonscroll.databinding.ItemTitlesBinding
 import com.rumit.tabchangeonscroll.models.TitleInfo
-import kotlinx.android.synthetic.main.item_titles.view.tvSubCatTextISF
 
 class TitlesAdapter(
-    val titles: ArrayList<TitleInfo?>,
+    val mContext: Context,
+    val titles: ArrayList<TitleInfo?>?,
     val onItemSelected: (selectedItemIndex: Int) -> Unit
-) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<TitlesAdapter.MyViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return SubCategoryViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_titles, parent, false)
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val binding: ItemTitlesBinding =
+            ItemTitlesBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+
+        return MyViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is SubCategoryViewHolder) {
-            holder.bind()
-        }
-    }
 
-    inner class SubCategoryViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        fun bind() {
-            titles[layoutPosition]?.let { model ->
-                itemView.tvSubCatTextISF.text = model.titleName
-                if (model.isSelected) {
-                    itemView.tvSubCatTextISF.setTextColor(
-                        ContextCompat.getColor(view.context, R.color.white)
-                    )
-                    itemView.tvSubCatTextISF.background =
-                        ContextCompat.getDrawable(view.context, R.drawable.unslected_button)
-                } else {
-                    itemView.tvSubCatTextISF.setTextColor(
-                        ContextCompat.getColor(view.context, R.color.black)
-                    )
-                    itemView.tvSubCatTextISF.background =
-                        ContextCompat.getDrawable(view.context, R.drawable.selected_button)
-                }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val model: TitleInfo? = titles?.get(holder.absoluteAdapterPosition)
 
-                itemView.tvSubCatTextISF.setOnClickListener {
-                    onItemSelected(layoutPosition)
-                }
+        if (model != null) {
+            holder.viewBinding.tvSubCatTextISF.text = model.titleName
+
+            if (model.isSelected) {
+                holder.viewBinding.tvSubCatTextISF.setTextColor(
+                    ContextCompat.getColor(mContext, R.color.white)
+                )
+                holder.viewBinding.tvSubCatTextISF.background =
+                    ContextCompat.getDrawable(mContext, R.drawable.unslected_button)
+            } else {
+                holder.viewBinding.tvSubCatTextISF.setTextColor(
+                    ContextCompat.getColor(mContext, R.color.black)
+                )
+                holder.viewBinding.tvSubCatTextISF.background =
+                    ContextCompat.getDrawable(mContext, R.drawable.selected_button)
+            }
+
+            holder.viewBinding.tvSubCatTextISF.setOnClickListener {
+                onItemSelected(holder.absoluteAdapterPosition)
             }
         }
     }
 
+
     override fun getItemCount(): Int {
-        return titles.size
+        return titles?.size ?: 0
     }
 
     fun updateList(position: Int) {
-        for ((index, model) in titles.withIndex()) {
+        for ((index, model) in titles?.withIndex()!!) {
             model?.isSelected = position == index
         }
         notifyDataSetChanged()
     }
+
+    open class MyViewHolder(val viewBinding: ItemTitlesBinding) :
+        RecyclerView.ViewHolder(viewBinding.root)
 }
