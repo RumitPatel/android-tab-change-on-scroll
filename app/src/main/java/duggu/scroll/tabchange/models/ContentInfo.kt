4b1a9f5c72311fcf91514581c6209c497e3fcf16
RumitPatel <rumit.patel5@gmail.com>
2023-12-26 12:10:41 +0530

    Initial project setup.

---
+++ app/src/main/java/duggu/scroll/tabchange/models/ContentInfo.kt
@@ -0,0 +1,7 @@
+package duggu.scroll.tabchange.models
+
+data class ContentInfo(
+    val contentType: Int? = 0,
+    val name: String? = "",
+    var index : Int = 0
+)
---
+++ app/src/main/java/duggu/scroll/tabchange/models/TitleInfo.kt
@@ -0,0 +1,6 @@
+package duggu.scroll.tabchange.models
+
+data class TitleInfo(
+    val titleName: String? = "",
+    var isSelected: Boolean = false
+)
